34c5a64c41eece92af914e560998cdb1f67e85e5
harelb <harel.biggie@colorado.edu>
2020-12-06 02:30:17 +0000

    Debugged

--- scripts/state_image.py
+++ scripts/state_image.py
@@ -1,53 +1,44 @@
+#!/usr/bin/env python
 import rospy
-from sensor_msgs import Image
-from std_msgs import *
+from sensor_msgs.msg import Image
+from std_msgs.msg import String
 from cv_bridge import CvBridge
 import cv2
 import os
 import numpy as np
-import argparse
 
-def parse_args():
-    pareser = argparse.ArgumentParser(
-        prog='state_image.py'
-        description='Publishes image of a given state'
-    )
-    parser.add_argument('img_folder', type=str, help='name of folder storing images', default = None, metavar = "Folder")
-    args = parser.parse_args()
-    return args
-
-class StateImagePubSub(folder):
-    def __init__(self):
+class StateImagePubSub():
+    def __init__(self, folder):
         # Params
         self.br = CvBridge()
-        self.image_left = cv2.imread(folder+'left_arrow.jpg', 1)
-        self.image_right = cv2.imread(folder+'right_arrow.jpg',1)
-        self.image_forward = cv2.imread(folder+'forward_arrow.jpg',1)
-        self.image_goal = cv2.imread(folder+'goal_point.jpg',1)
-        
-        self.loop_rate = rospy.Rate(10)
-        self.state_string = None
+        self.image_left = cv2.imread('/home/arpg/spot_ws/src/spot_image_state/img/right_arrow.jpg', 1)
+        self.image_right = cv2.imread('/home/arpg/spot_ws/src/spot_image_state/img/left_arrow.jpg',1)
+        self.image_forward = cv2.imread('/home/arpg/spot_ws/src/spot_image_state/img/forward_arrow.jpg',1)
+        self.image_goal = cv2.imread('/home/arpg/spot_ws/src/spot_image_state/img/goal_point.jpg',1)
+        # print type(self.image_left)
 
-        self.pub = rospy.publish('state_image', Image, queue_size=10)
-        rospy.Subscriber("state", String(), state_cb)
+        self.loop_rate = rospy.Rate(10)
+        self.statestring = None
+
+        self.pub = rospy.Publisher('state_image', Image, queue_size=10)
+        rospy.Subscriber('/spot_joy_interface/state', String, self.state_cb)
     
     def state_cb(self,msg):
-        self.state_string = msg.data
+        self.statestring = msg.data
 
     def loop(self):
         while not rospy.is_shutdown():
-            if self.statestring == "left":
-                self.pub.publish(br.cv2_to_imgmsg(self.image_left))
-            elif self.statestring == "right":
-                self.pub.publish(br.cv2_to_imgmsg(self.image_right))
-            elif self.statestring == "goal":
-                self.pub.publish(br.cv2_to_img_msgs(self.image_goal))
+            if self.statestring == "left_turn":
+                self.pub.publish(self.br.cv2_to_imgmsg(self.image_left))
+            elif self.statestring == "right_turn":
+                self.pub.publish(self.br.cv2_to_imgmsg(self.image_right))
+            elif self.statestring == "at_goal":
+                self.pub.publish(self.br.cv2_to_imgmsg(self.image_goal))
             else:
-                self.pub.publish(br.cv2_to_img_msgs(self.image_forward))
+                self.pub.publish(self.br.cv2_to_imgmsg(self.image_forward))
             self.loop_rate.sleep()
 
 if __name__ == "__main__":
-    args = parse_args()
     rospy.init_node("image_state", anonymous=True)
-    my_node = StateImagePubSub(folder=args.Folder)
+    my_node = StateImagePubSub(folder='/home/arpg/spot_ws/spot_image_state/img/')
     my_node.loop()
